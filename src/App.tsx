fd82f9426e780ad4a53dafcc2258d1d91332651c
CurpraCode <curtsoyaks@gmail.com>
2023-08-27 19:52:20 +0100

    feat: 🎸 create the auth with aws amplify react ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator, View, Card } from '@aws-amplify/ui-react';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-     <p>Welcome to setup of React.js and AWS amplify</p>
-    </div>
+    <View className="App">
+      <Card>
+             <Home/>
+      </Card>
+
+    </View>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
